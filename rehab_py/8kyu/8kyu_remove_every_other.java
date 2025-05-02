3dba48fd5a75e5682563e00f4ae0f6bfe1981396
RaphaFang <fangsihyu0211@gmail.com>
2025-05-03 00:12:49 +0800

    add: removeEveryOther func

---
+++ rehab_py/8kyu/8kyu_remove_every_other.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Kata {
+    public static Object[] removeEveryOther(Object[] arr) {
+        List<Object> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; i += 2) {
+            result.add(arr[i]);
+        }
+        return result.toArray();
+    }
+}
---
+++ rehab_py/8kyu/8kyu_remove_every_other.py
@@ -0,0 +1,6 @@
+def remove_every_other(my_list):
+    return my_list[::2]
+
+
+# aaa = [0,1,2,3,4,5,6,7,8,9]
+# print(aaa[::2])
