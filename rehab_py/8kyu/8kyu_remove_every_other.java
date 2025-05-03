1510f5183b74daa3d4d71fe4557e37ddb7f45361
RaphaFang <fangsihyu0211@gmail.com>
2025-05-03 15:22:28 +0800

    optimize: use Object[] instead of List<Object>, when i can makesure the amount of item in it

--- rehab_py/8kyu/8kyu_remove_every_other.java
+++ rehab_py/8kyu/8kyu_remove_every_other.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Kata {
+class Kata_original {
     public static Object[] removeEveryOther(Object[] arr) {
         List<Object> result = new ArrayList<>();
         for (int i = 0; i < arr.length; i += 2) {
@@ -10,3 +10,22 @@ class Kata {
         return result.toArray();
     }
 }
+
+class Kata_optimized {
+    public static Object[] removeEveryOther(Object[] arr) {
+        int size = (arr.length + 1) / 2;
+        Object[] result = new Object[size];
+
+        for (int i = 0, j = 0; i < arr.length; i += 2, j += 1) {
+            result[j] = arr[i];
+        }
+        return result;
+    }
+}
+
+// !1. optimized
+// 要基於java的資料結構
+// Object[] 比 List<Object> 快，但是必須一次性寫死[]內部有的元件數量
+// List 可以用 .add()
+// Object[] 只能用 arr[i]= x
+// 但是List底層仍然是Object，List會設定一個政列數量，不夠用時會自動複製
