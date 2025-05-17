852edcb2fea62a4a4949f9b103f45ed47a64d08c
RaphaFang <fangsihyu0211@gmail.com>
2025-05-18 00:13:24 +0800

    add: 283, the func to resort 0 in list

---
+++ leet/two_pointer/move_zeroes_283.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        zero_list = []
+        c_list = []
+        
+        for i in range(len(nums)):
+            zero_list.append(0) if nums[i]==0 else c_list.append(nums[i])                
+             
+        nums.clear()
+        c_list.extend(zero_list)
+        nums.extend(c_list)
+
+
+aa = Solution()
+aa.moveZeroes([0,1,0,3,12])
+
+
+# c_list.pop(i) 
+# 不能直接在複製的list推掉東西，會修改到這複製品的長度
