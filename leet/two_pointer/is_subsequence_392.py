054382cfabc517aa040e63548b79fb19841996bf
RaphaFang <fangsihyu0211@gmail.com>
2025-05-19 17:48:31 +0800

    add: challenge solved, without gpt

--- leet/two_pointer/is_subsequence_392.py
+++ leet/two_pointer/is_subsequence_392.py
@@ -1,10 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         temp = 0
+        t_copy = list(t)
+
         for i in range(len(s)):
-            if s[i] in t[i:]:
-                dot = t.index(s[i]) + i
-                print(t[i:], t.index(s[i]) , dot)
+            if s[i] in t_copy[temp:]:
+                dot = t_copy[temp:].index(s[i]) + temp
+                t_copy[dot] = "0"
+
                 if dot< temp:
                     return False
                 else:
@@ -12,26 +15,7 @@ class Solution:
             else:
                 return False
         return True
-            # print(dot)
-
-        # index_list = []
-        # for _ in range(len(s)):
-        #     if s[_] not in t:
-        #         return False
-        #     else:
-        #         print(t[_:])
-        #         print(_, type(t[_:].index(s[_])))
-                
-        #         index_list.append(t[_:].index(s[_]))
-
-        # print(index_list)
-
-        # for n in range(len(index_list)-1):
-        #     if index_list[n] > index_list[n+1]:
-        #         return False
-
-        # return True
-
+    
 
 aa = Solution()
-print(aa.isSubsequence(s = "aaaaaa", t = "bbaaa"))
+print(aa.isSubsequence(s = "aaaaa", t = "bbaaaa"))
