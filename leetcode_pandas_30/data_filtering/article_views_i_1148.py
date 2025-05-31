bb99940b6a63134670071cc9a7b690c9fc612f5d
方思渝 <fangsihyu0211@gmail.com>
2025-05-31 22:10:37 +0800

    add: pd for 1148

---
+++ leetcode_pandas_30/data_filtering/article_views_i_1148.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    r = views[views['author_id']== views['viewer_id']][['author_id']]
+    return r.drop_duplicates().sort_values().rename(columns={'author_id':'id'})
+
+# .sort_values(by='author_id')
+# 如果是Series，不用特別說特定哪一欄位，因為我這邊只有一個
+# 但是是dataframe 就要
+
+# 我這邊回傳是dataframe 就要
