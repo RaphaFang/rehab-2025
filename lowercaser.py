38837b41084a8967e44377fe0d49522cfa57c890
方思渝 <fangsihyu0211@gmail.com>
2025-05-31 22:22:27 +0800

    add: pd for 1683

--- lowercaser.py
+++ lowercaser.py
@@ -2,7 +2,7 @@ def lowercaser(item, quize_id):
     print(item.lower().replace(" ", "_") + "_" + str(quize_id) + ".")
         
 
-lowercaser("Article Views I", 1148)
+lowercaser("Invalid Tweets", 1683)
 
 
 # ----------------------------------------------------------------------------
