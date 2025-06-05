96258c9b19ea0276571b6bd5052b428843a279aa
方思渝 <fangsihyu0211@gmail.com>
2025-06-05 15:46:29 +0800

    add: pd for 1795, with comment about UNION ALL, and UNPIVOT

--- leetcode_pandas_30/data_manipulation/rearrange_products_table_1795.py
+++ leetcode_pandas_30/data_manipulation/rearrange_products_table_1795.py
@@ -7,3 +7,22 @@ def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
 
 
 # wide form, long form
+
+
+# ! sql沒有melt()，要透過 UNION ALL
+# mysql 沒有 UNPIVOT ，所以要透過 UNION ALL 模擬
+# SELECT product_id, 'store1' AS store, store1 AS price
+# FROM Products
+# WHERE store1 IS NOT NULL
+
+# UNION ALL
+
+# SELECT product_id, 'store2' AS store, store2 AS price
+# FROM Products
+# WHERE store2 IS NOT NULL
+
+# UNION ALL
+
+# SELECT product_id, 'store3' AS store, store3 AS price
+# FROM Products
+# WHERE store3 IS NOT NULL;
