6ff8d99a731040e8fd1cc6c4e97fa5ec1d0bb9f0
方思渝 <fangsihyu0211@gmail.com>
2025-06-23 00:26:14 +0800

    add: pd for 1484, with more comment about the different between approch

--- leetcode_pandas_30/data_aggregation/group_sold_products_by_the_date_1484.py
+++ leetcode_pandas_30/data_aggregation/group_sold_products_by_the_date_1484.py
@@ -1,20 +1,27 @@
 import pandas as pd
 
 def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
-    return activities.groupby("sell_date")["product"].agg([
-        ("num_sold","nunique"),
-        ("products", lambda x: ",".join(sorted(x.unique())))
-    ]).reset_index()
+    return (activities
+            .groupby("sell_date", as_index=False)
+            .agg(
+                num_sold = ("product","nunique"),
+                products = ("product", lambda x: ",".join(sorted(x.unique())))
+            ))
 
+
+# --------------------------------------------------------------------------------
 def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
     r = (activities
          .drop_duplicates()
-        #  .sort_values('product')
          .groupby("sell_date", as_index=False)
          .agg(
              num_sold = ('product', 'count'),
-             products = ('product', lambda x: ','.join(x))
-         )
-         .rename(columns = {'product':'num_sold'}))
+             products = ('product', lambda x: ','.join(sorted(x)))
+         ))
     return r
 
+# --------------------------------------------------------------------------------
+# 下面的寫法會快一點
+# 因為，drop_duplicates() 下面只做一次，但是上面做了兩次，分別在agg()裡面的兩個欄位
+
+# 要統一寫agg()的方式，先命名欄位，接著傳入舊欄位，以及希望施加的函數
