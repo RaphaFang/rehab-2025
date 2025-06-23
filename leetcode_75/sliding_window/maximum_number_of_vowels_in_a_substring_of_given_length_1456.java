45a9b884e70955058f5049bfdfc9d5475878c3a0
方思渝 <fangsihyu0211@gmail.com>
2025-06-23 12:23:33 +0800

    add: java fpr 1456, add early stopping for both py and java

--- leetcode_75/sliding_window/maximum_number_of_vowels_in_a_substring_of_given_length_1456.java
+++ leetcode_75/sliding_window/maximum_number_of_vowels_in_a_substring_of_given_length_1456.java
@@ -1,5 +1,32 @@
 package leetcode_75.sliding_window;
 
-public class maximum_number_of_vowels_in_a_substring_of_given_length_1456 {
+import java.util.Set;
 
+class Solution {
+    public int maxVowels(String s, int k) {
+        Set<Character> vowel = Set.of('a', 'e', 'i', 'o', 'u');
+
+        int left = 0, right = k - 1, temp = 0;
+
+        for (int i = left; i <= right; i++) {
+            if (vowel.contains(s.charAt(i))) {
+                temp++;
+            }
+        }
+
+        int m_vowel = temp;
+
+        while (right < s.length() - 1) {
+            if (vowel.contains(s.charAt(left))) {
+                temp--;
+            }
+            left++;
+            right++;
+            if (vowel.contains(s.charAt(right))) {
+                temp++;
+            }
+            m_vowel = Math.max(m_vowel, temp);
+        }
+        return m_vowel;
+    }
 }
