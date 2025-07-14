1315c0222c70800fa9edc08ab1e8a63aa519a810
方思渝 <fangsihyu0211@gmail.com>
2025-07-14 17:37:28 +0800

    add: java for 735

--- leetcode_75/stack/asteroid_collision_735.java
+++ leetcode_75/stack/asteroid_collision_735.java
@@ -1,5 +1,33 @@
 package leetcode_75.stack;
 
-public class asteroid_collision_735 {
+import java.util.*;
 
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int asteroid : asteroids) {
+            boolean alive = true;
+            while (alive && !stack.isEmpty() && stack.peekLast() > 0 && asteroid < 0) {
+                if (stack.peekLast() + asteroid < 0) {
+                    stack.pollLast(); // 當前右邊小行星比左邊還弱，被吃掉
+                } else if (stack.peekLast() + asteroid == 0) {
+                    stack.pollLast(); // 兩者抵銷
+                    alive = false;
+                } else {
+                    alive = false;
+                }
+            }
+            if (alive) {
+                stack.addLast(asteroid);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        int i = 0;
+        for (int val : stack) {
+            result[i++] = val;
+        }
+        return result;
+    }
 }
