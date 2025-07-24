9bd132f765721b1c1db57603aabf6468a6e003b3
方思渝 <fangsihyu0211@gmail.com>
2025-07-24 23:53:19 +0800

    add: keep trying for 726 hard

--- leetcode_hard/stack/number_of_atoms_726.py
+++ leetcode_hard/stack/number_of_atoms_726.py
@@ -1,30 +1,29 @@
 from collections import deque, defaultdict
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
-        num_dq = deque()
-        ele_dq = deque()
-        num_holder, ele_holder = [], []
-
+        num_dq, ele_dq, num_holder, ele_holder = deque(), deque(), deque(), deque()
         backward = False
-        ele_dict = defaultdict()
 
         for ch in formula:
             if ch.isdigit():
                 if backward == False:
                     rebuild = num_dq.pop()
-                    if rebuild != 1:
+                    if rebuild != '1':
+
                         num_dq.append(rebuild * 10 + int(ch))
                     else:
                         num_dq.append(int(ch))
                 
                 if backward:
-                    if ele_dq[-1] == "(":
-                        backward = False
-                        break
-
-                    num_holder.append(num_dq.pop() * int(ch))
-                    ele_holder.append(ele_dq.pop())
-
+                    while ele_dq and ele_dq[-1] != "(":
+                        num_holder.appendleft(int(num_dq.pop()) * int(ch))
+                        ele_holder.appendleft(ele_dq.pop())
+                    ele_dq.pop()
+                    num_dq.extend(num_holder)
+                    ele_dq.extend(ele_holder)
+                    num_holder.clear()
+                    ele_holder.clear()
+                    backward = False
 
             elif ch == ')':
                 backward = True
@@ -34,14 +33,34 @@ class Solution:
             
             elif ch.isupper():
                 ele_dq.append(ch)
-                num_dq.append(1) # 至少會有1
+                num_dq.append('1') # 至少會有1
 
             elif ch.islower():
                 rebuild = ele_dq.pop()
                 ele_dq.append(rebuild + ch)
 
-            
+        print(ele_dq)
+        print(num_dq)
+        ele_dict = defaultdict(int)
+        for i in range(len(ele_dq)):
+            ele_dict[ele_dq[i]] += int(num_dq[i])
+
+        result = ''
+        for n in sorted(ele_dict.items()):
+            if n[1] > 1:
+                result += (n[0] + str(n[1]))
+            else:
+                result += n[0]
+        print(result)
+        return result
+
+
+        
 
 
 
-             
+ss = Solution()
+ss.countOfAtoms("K4(ON(SO3)2)2")
+ss.countOfAtoms("Mg(OH)2")
+ss.countOfAtoms("(NB3)33")
+# ss.countOfAtoms('H11He49NO35B7N46Li20')
