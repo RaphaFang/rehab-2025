83d6ceb5bdedde6ead449271982fad0d81e05901
方思渝 <fangsihyu0211@gmail.com>
2025-07-28 18:45:18 +0800

    add: py for 726(hard), with almost 12 hours dealing with this..., could've done it enev faster if i recognize the need of data cleaning in first place.

--- leetcode_hard/stack/number_of_atoms_726.py
+++ leetcode_hard/stack/number_of_atoms_726.py
@@ -1,12 +1,19 @@
 from collections import deque, defaultdict
+import re
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
         num_dq, ele_dq = deque(), deque()
 
+        # data cleaning part
+        print(formula)
+        formula = re.sub(r'\)(?!\d)', ')1', formula)
+        print(formula)
+
+
         for ch in formula:
             if ch.isdigit():
                 rebuild = num_dq.pop()
-
+                
                 if rebuild == ')':
                     num_dq.append(rebuild)
 
@@ -34,47 +41,52 @@ class Solution:
                 rebuild = ele_dq.pop()
                 ele_dq.append(rebuild + ch)
         
-            # if backward:
-            #         while ele_dq and ele_dq[-1] != "(":
-            #             num_holder.appendleft(int(num_dq.pop()) * int(ch))
-            #             ele_holder.appendleft(ele_dq.pop())
-            #         ele_dq.pop()
-            #         num_dq.extend(num_holder)
-            #         ele_dq.extend(ele_holder)
-            #         num_holder.clear()
-            #         ele_holder.clear()
-            #         backward = False
-
-        print(ele_dq)
-        print(num_dq)
-
+        # 這邊還需要有對列，去處理 backward
         temp_dq = deque()
-        num_holder = deque()
-        ele_dict = defaultdict(int)
+        backward_holder = deque()
+        backward = False
 
         for i in range(len(ele_dq)):
-            if ele_dq[i] in ('(', ')'):
-                temp_dq.append(ele_dq[i])
-            elif ele_dq[i] == 'nnn':
-                num_holder.append(num_dq[i])
-            ele_dict[ele_dq[i]] += int(num_dq[i])
+            while temp_dq and backward == True:  # 要確保他跳到下一數字時再來處理backward
+                h = temp_dq.pop()
+                if h[0] == '(':
+                    temp_dq.extend(backward_holder)
+                    backward_holder.clear()
+                    backward = False
+                else:                    
+                    backward_holder.appendleft((h[0], int(h[1]) * int(num_dq[i])))
 
-        # result = ''
-        # for n in sorted(ele_dict.items()):
-        #     if n[1] > 1:
-        #         result += (n[0] + str(n[1]))
-        #     else:
-        #         result += n[0]
-        # print(result)
-        # return result
+            if ele_dq[i] == ')':
+                backward = True
+            elif ele_dq[i] == 'nnn':
+                pass
+            else:
+                temp_dq.append((ele_dq[i], num_dq[i]))
+        # print(temp_dq)
+
+        r = defaultdict(int) #! 這邊要加入預設數值 (int, str, ...)，不然報錯
+        for k, v in temp_dq:
+            r[k] += int(v)
+
+        result = ''
+        for n in sorted(r.keys()):
+            if int(r[n]) == 1:
+                result += n
+            else:
+                result += (n + str(r[n]))
+        print(result)
+        return result
 
 
 ss = Solution()
-# ss.countOfAtoms("K4(ON(SO3)2)27")
-# ss.countOfAtoms("Mg(OH)2")
+ss.countOfAtoms("Fe(CN)F6")
+# ss.countOfAtoms("(((K4(ON(SO34)2)3(Fe(CN)F6)22)4(Mg(OH)2)10)2(H2O)5)13")
+# ss.countOfAtoms("Mg(OH)1")
+# ss.countOfAtoms("(Mg)(OH)")
+
 # ss.countOfAtoms("(NB3)33")
 # ! 這邊會出問題，是因為我設定的backward 沒有讓數字建立好
 # ! 應該要是雙位數的，但是只建立了一位數，就相成進去了
 # ! 導致數子就算的不對了
 
-ss.countOfAtoms('H11He49NO35B7N46Li20')
+# ss.countOfAtoms('H11He49NO35B7N46Li20')
