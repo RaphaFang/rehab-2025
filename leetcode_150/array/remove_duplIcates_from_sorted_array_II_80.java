b1c599772610e8e672b1bca0a6f23986b44ee461
方思渝 <fangsihyu0211@gmail.com>
2025-08-06 17:16:21 +0800

    add: java for 80

--- leetcode_150/array/remove_duplIcates_from_sorted_array_II_80.java
+++ leetcode_150/array/remove_duplIcates_from_sorted_array_II_80.java
@@ -1,5 +1,21 @@
-package leetcode_150.array;
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        var k = 1;
+        var c = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                nums[k] = nums[i];
+                c = 1;
+                k++;
+            } else if (nums[i] == nums[i - 1]) {
+                if (c < 2) {
+                    nums[k] = nums[i];
+                    c++;
+                    k++;
+                }
 
-public class remove_duplIcates_from_sorted_array_II_80 {
-
+            }
+        }
+        return k;
+    }
 }
